2a2e141d53a5344f14f2f18cf543d7e992bb3745
Nhathao1907 <lenhathao19072001@gmail.com>
2023-11-19 05:24:13 +0700

    update danh muc

---
+++ admin/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  
+</head>
+<body>
+    <div class="boxcenter">
+        <div class="row mb headeradmin">
+            <h1>QUẢN TRỊ WEBSITE</h1>
+        </div>
+        <div class="row mb">
+            <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+                <ul class="navbar-nav">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Trang chủ</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="index.php?act=adddm">Danh mục</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="index.php?act=addsp">Hàng hóa</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="index.php?act=dskh">Khách hàng</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="index.php?act=dsbl">Bình luận</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="index.php?act=thongke">Thống kê</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="index.php?act=dsdh">Danh sách đơn hàng</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+  
